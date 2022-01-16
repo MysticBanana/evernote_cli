8bae216d4443fb8f75f97e1c7bb45f88fe85dbba
tschaed1 <tschaed1@hs-mittweida.de>
2022-01-16 17:31:41 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+import sys
+
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+install_modules = ["evernote", "cryptography.fernet", "enum", "enum32", "httplib2", "ipaddress", "oauth2", "stdiomask"]
+
+setuptools.setup(
+    name="evernote_cli",
+    version="1.0",
+    author="Secret",
+    author_email="no-reply@hs-mittweida.de",
+    description="Package to create Hive",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='<=2.7',
+)
