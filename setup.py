a59377c85bcc6eb16c095ebfc90dd802a0c36008
hans <email@email.mail>
2022-01-23 20:44:23 +0100

    requirements.txt

--- setup.py
+++ setup.py
@@ -6,7 +6,13 @@ import sys
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-install_modules = ["evernote", "cryptography", "enum34", "httplib2", "ipaddress", "oauth2", "stdiomask"]
+install_modules = ["evernote==1.25.3",
+                    "enum34==1.1.10",
+                    "cryptography==3.3.2",
+                    "httplib2==0.20.2",
+                    "ipaddress==1.0.23",
+                    "oauth2==1.9.0.post1"
+]
 
 setuptools.setup(
     name="evernote_cli",
