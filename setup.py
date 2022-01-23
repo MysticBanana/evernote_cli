33888cbcb523944dc6df3bccbf9825a9f23ac862
hans <email@email.mail>
2022-01-23 20:56:38 +0100

    requirements.txt

--- setup.py
+++ setup.py
@@ -6,17 +6,11 @@ import sys
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-install_modules = ["evernote==1.25.3",
-                    "enum34==1.1.10",
-                    "cryptography==3.3.2",
-                    "httplib2==0.20.2",
-                    "ipaddress==1.0.23",
-                    "oauth2==1.9.0.post1"
-]
+install_modules = ["evernote", "cryptography", "enum34", "httplib2", "ipaddress"]
 
 setuptools.setup(
     name="evernote_cli",
-    version="1.0.1",
+    version="1.0.3",
     author="Secret",
     author_email="no-reply@hs-mittweida.de",
     description="Package to create Hive",
