d9b0784e848610541c472975e6122a1ea506de6e
MysticBanana <89936555+MysticBanana@users.noreply.github.com>
2022-01-27 18:25:19 +0100

    fixed file does not exist when caling from other path

--- evernote_cli.py
+++ evernote_cli.py
@@ -23,6 +23,9 @@ class Evernote:
         self._testing = True
         self._easter_egg = False
 
+        # change working dir to current dir
+        os.chdir(os.path.realpath(__file__))
+
         self.global_data_manager = global_data_manager.GlobalFileManager(self)
 
         self.log_level = params.get("log_level", logging.INFO)
@@ -314,6 +317,7 @@ if __name__ == "__main__":
     tmp_user_password = "test"
 
     token = "S=s1:U=96801:E=1845cafec40:C=17d04fec040:P=185:A=mneuhaus:V=2:H=ce322afcd49b909aadff4e59c4354924"
+
     # e = Evernote(
     #       "-u {user_name} -p {password} -c -e 0".format(user_name=tmp_user_name, password=tmp_user_password, token=token).split(" "))
     # e = Evernote("-h -p".format(user_name=tmp_user_name, password=tmp_user_password).split(" "))
